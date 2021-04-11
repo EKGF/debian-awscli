3eba3d13c33a9320e58491768de1fbd75c82cd8a
Stefan Szaniszlo <stefan.szaniszlo@agnos.ai>
2021-04-11 16:42:12 +0100

    bump version to 0.0.24 in order to force image push to dockerhub

--- localbuild.sh
+++ localbuild.sh
@@ -2,7 +2,7 @@
 _IMAGE_NAME="docker.io/ekgf/debian-awscli"
 _IMAGE_VERSION="latest"
 # also bump the  version in .github/workflows/build.yml
-_MANUALLY_INCREMENTED_IMAGE_VERSION="0.0.23"
+_MANUALLY_INCREMENTED_IMAGE_VERSION="0.0.24"
 
 docker build . \
   --iidfile=image.id \
